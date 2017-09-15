e2ffeb3da88852f892da0cb45e5dc51e638b3066
ChibiFR <23081995b@gmail.com>
2017-09-15 05:22:36 +0200

    Add inputs index

--- src/input/Keyboard.ts
+++ src/input/Keyboard.ts
@@ -2,14 +2,14 @@ export abstract class Keyboard {
   // protected static keys: Key[];
 
   // public static _init(): void {
-    // this.keys = [];
+  //   this.keys = [];
 
-    // window.addEventListener('keydown', (e: KeyboardEvent): void => {
-      // const index = this.keys.indexOf(e.keyCode);
+  //   window.addEventListener('keydown', (e: KeyboardEvent): void => {
+  //     const index = this.keys.indexOf(e.keyCode);
 
-      // if (index !== -1) {
-      //   this.keys[index] = true;
-      // }
-    // });
-  }
+  //     if (index !== -1) {
+  //       this.keys[index] = true;
+  //     }
+  //   });
+  // }
 }
