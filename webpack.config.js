ce838046be39b99659c22278a1ed7d0f6ceb569a
ChibiFR <23081995b@gmail.com>
2017-09-17 02:34:58 +0200

    Remove ts config

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,12 @@ const path = require('path');
 const BabelMinify = require('babel-minify-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname, 'src', 'index.ts'),
+  entry: path.join(__dirname, 'src', 'index.js'),
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'app.js'
   },
   devtool: 'source-map',
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  module: {
-    rules: [
-      { test: /\.ts/, loader: 'awesome-typescript-loader' },
-      { enforce: 'pre', test: /\.js/, loader: 'source-map-loader' }
-    ]
-  },
   plugins: [
     new BabelMinify()
   ]
